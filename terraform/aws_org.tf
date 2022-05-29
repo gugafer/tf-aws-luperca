bd6498c8614abcda2426270e29c3666cc6e0a3ac
ricardo-f <riclima1990@gmail.com>
2022-05-29 01:01:41 -0300

    the org resources

---
+++ terraform/aws_org.tf
@@ -0,0 +1,34 @@
+resource "aws_organizations_organization" "root" {
+  aws_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com",
+  ]
+
+  feature_set          = "ALL"
+  enabled_policy_types = ["SERVICE_CONTROL_POLICY"]
+}
+
+resource "aws_organizations_organizational_unit" "active" {
+  for_each  = var.business_unit
+  name      = each.key
+  parent_id = aws_organizations_organization.root.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "trash" {
+  name      = "trash"
+  parent_id = aws_organizations_organization.root.roots[0].id
+}
+
+resource "aws_organizations_account" "account" {
+  for_each = {
+    for buenv in local.buEnv : "${buenv.buName}-${buenv.env}" => buenv
+  }
+  name              = "${each.value.buName}-${each.value.env}"
+  email             = "riclima1990+${each.value.buName}-${each.value.env}@gmail.com"
+  parent_id         = aws_organizations_organizational_unit.active[each.value.buName].id
+  close_on_deletion = true
+
+  lifecycle {
+    ignore_changes = [role_name]
+  }
+}
