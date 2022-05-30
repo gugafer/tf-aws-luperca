044ffcd2f1577f20a6cdd5346a2f0b786f661ad6
ricardo-f <riclima1990@gmail.com>
2022-05-30 15:47:34 -0300

    adding new policy

--- terraform/svc_control_policies.tf
+++ terraform/svc_control_policies.tf
@@ -136,3 +136,77 @@ resource "aws_organizations_policy_attachment" "tmp" {
   policy_id = aws_organizations_policy.tmp.id
   target_id = aws_organizations_organization.root.roots[0].id
 }
+
+resource "aws_organizations_policy" "dsous" {
+  name = "Deny Services Outside US"
+
+  content = <<CONTENT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "DenyAllOutsideUS",
+            "Effect": "Deny",
+            "NotAction": [
+                "a4b:*",
+                "acm:*",
+                "aws-marketplace-management:*",
+                "aws-marketplace:*",
+                "aws-portal:*",
+                "budgets:*",
+                "ce:*",
+                "chime:*",
+                "cloudfront:*",
+                "config:*",
+                "cur:*",
+                "directconnect:*",
+                "ec2:DescribeRegions",
+                "ec2:DescribeTransitGateways",
+                "ec2:DescribeVpnGateways",
+                "fms:*",
+                "globalaccelerator:*",
+                "health:*",
+                "iam:*",
+                "importexport:*",
+                "kms:*",
+                "mobileanalytics:*",
+                "networkmanager:*",
+                "organizations:*",
+                "pricing:*",
+                "route53:*",
+                "route53domains:*",
+                "s3:GetAccountPublic*",
+                "s3:ListAllMyBuckets",
+                "s3:PutAccountPublic*",
+                "shield:*",
+                "sts:*",
+                "support:*",
+                "trustedadvisor:*",
+                "waf-regional:*",
+                "waf:*",
+                "wafv2:*",
+                "wellarchitected:*"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringNotEquals": {
+                    "aws:RequestedRegion": [
+                        "af-*",
+                        "ap-*", 
+                        "ca-*", 
+                        "eu-*", 
+                        "me-*", 
+                        "sa-*" 
+                    ]
+                }
+            }
+        }
+    ]
+}
+CONTENT
+}
+
+resource "aws_organizations_policy_attachment" "dsous" {
+  policy_id = aws_organizations_policy.dsous.id
+  target_id = aws_organizations_organization.root.roots[0].id
+}
