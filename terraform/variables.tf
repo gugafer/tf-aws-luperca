ea0011ad670dc3f2c046b2560115a9826ef62b6f
ricardo-f <riclima1990@gmail.com>
2022-06-01 20:34:53 -0300

    creating prd acct to bu1 and bu2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "business_unit" {
   description = "Name of the business unit and the needed environments"
   default = {
-    bu1 = [],
-    bu2 = []
+    bu1 = ["prd"],
+    bu2 = ["prd"]
   }
 }
